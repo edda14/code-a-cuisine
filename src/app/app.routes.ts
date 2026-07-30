7ccab53c3c6cca855212bfc653b29db0eb60fb02
Julia Schäffer <juliaschaeffer96@gmail.com>
2026-07-30 20:46:15 +0200

    add results

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,23 +2,30 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { GenerateRecipe } from './pages/generate-recipe/generate-recipe';
 import { Preferences } from './pages/preferences/preferences';
-import {Loading } from './pages/loading/loading';
+import { Loading } from './pages/loading/loading';
+import { Results } from './pages/results/results';
+
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
   },
-    {
+  {
     path: 'generate-recipe',
     component: GenerateRecipe,
   },
-    {
+  {
     path: 'preferences',
     component: Preferences,
   },
-     {
+  {
     path: 'loading',
     component: Loading,
   },
+  {
+    path: 'results',
+    loadComponent: () =>
+      import('./pages/results/results').then((m) => m.Results)
+  }
 ];
--- src/app/pages/loading/loading.ts
+++ src/app/pages/loading/loading.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,14 +8,40 @@ import { Router } from '@angular/router';
   templateUrl: './loading.html',
   styleUrl: './loading.scss',
 })
-
-export class Loading {
-  constructor(private router: Router) { }
+export class Loading implements OnInit {
   animationFinished = false;
+
+  constructor(
+    private router: Router,
+    private http: HttpClient
+  ) { }
+
+  ngOnInit(): void {
+    this.testN8nConnection();
+    setTimeout(() => {
+      this.router.navigate(['/results']);
+    }, 8000);
+  }
+
+  private testN8nConnection(): void {
+    const url = '/webhook-test/generate-recipes';
+
+    const body = {
+      message: 'Hallo n8n'
+    };
+
+    this.http.post<{ message: string }>(url, body).subscribe({
+      next: (response) => {
+        console.log('Antwort von n8n:', response);
+      },
+      error: (error) => {
+        console.error('Fehler bei n8n:', error);
+      }
+    });
+  }
 
   onAnimationEnd(): void {
     this.animationFinished = true;
     console.log('Animation ist fertig');
-    // this.router.navigate(['/recipe', recipeId]);
   }
 }
